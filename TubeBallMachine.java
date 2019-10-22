6d31f21c6a76f4d80068d339466d0f95147d7749
vanderson.nery <vanderson.nery@edu.pucrs.br>
2019-10-21 22:15:54 -0300

    Classe principal e VS Code

---
+++ TubeBallMachine.java
@@ -0,0 +1,10 @@
+/**
+ * TubeBallMachine
+ */
+public class TubeBallMachine {
+
+    public static void main(String[] args) {
+        System.out.println("Trabalho 2 Algoritmo e Estrutura de Dados");
+        System.out.println("Autores: Arthur, Cassius, Vanderson");
+    }
+}
